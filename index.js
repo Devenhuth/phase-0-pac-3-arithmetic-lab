f2dc42242327c2d51f44361ea4f77c0f80db4f4a
Deven <devenzachuth@gmail.com>
2022-02-24 22:47:05 -0600

    commit tree ff5bf6fa032f4c7e3ea0cdd3ce9a86393f13baab

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b
+}
+add (2, 4)
+
+function subtract(a, b){
+    return a - b
+}
+subtract (45, 12)
+
+function multiply(a, b){
+    return a * b
+}
+multiply (43, 54)
+
+function divide(a, b){
+    return a / b
+}
+divide (45, 5)
+ 
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
